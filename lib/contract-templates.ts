df10b12c77780bdd3508ffd07052a4df7b7183ca
Adam Farrell <joyfuladam@me.com>
2026-02-04 08:38:03 -0500

    Remove underscores from signature/date lines per user request

--- lib/contract-templates.ts
+++ lib/contract-templates.ts
@@ -50,16 +50,16 @@ IN WITNESS WHEREOF, the parties have executed this Agreement as of the Effective
 
 **Publisher:** River and Ember, LLC  
 
-Signature: _____________________________ <span style="color: white;">{{signature:1:y}}</span>  
+Signature: <span style="color: white;">{{signature:1:y}}</span>  
 Name: {{publisher_manager_name}}  
 Title: {{publisher_manager_title}}  
-Date: _______________ <span style="color: white;">{{date:1:y}}</span>
+Date: <span style="color: white;">{{date:1:y}}</span>
 
 **Writer:**  
 
-Signature: _____________________________ <span style="color: white;">{{signature:2:y}}</span>  
+Signature: <span style="color: white;">{{signature:2:y}}</span>  
 Name: {{writer_full_name}}  
-Date: _______________ <span style="color: white;">{{date:2:y}}</span>
+Date: <span style="color: white;">{{date:2:y}}</span>
 
 **Exhibit A: Compositions**
 
@@ -110,15 +110,15 @@ IN WITNESS WHEREOF, the parties have executed this Agreement as of the Effective
 
 **Label:** River and Ember, LLC  
   
-Signature: _____________________________ <span style="color: white;">{{signature:1:y}}</span>  
+Signature:  <span style="color: white;">{{signature:1:y}}</span>  
 Name: {{publisher_manager_name}}  
 Title: {{publisher_manager_title}}  
-Date: _______________ <span style="color: white;">{{date:1:y}}</span>
+Date:  <span style="color: white;">{{date:1:y}}</span>
 
 {% if is_musician %}**Musician:**{% else %}{% if is_producer %}**Producer:**{% else %}**Artist:**{% endif %}{% endif %}  
-Signature: _____________________________ <span style="color: white;">{{signature:2:y}}</span>  
+Signature:  <span style="color: white;">{{signature:2:y}}</span>  
 Name: {{artist_full_name}}  
-Date: _______________ <span style="color: white;">{{date:2:y}}</span>
+Date:  <span style="color: white;">{{date:2:y}}</span>
 
 **Exhibit A: Song & Recording Details**
 
--- templates/contracts/publishing-assignment.md
+++ templates/contracts/publishing-assignment.md
@@ -37,16 +37,16 @@ IN WITNESS WHEREOF, the parties have executed this Agreement as of the Effective
 
 **Publisher:** River and Ember, LLC  
 
-Signature: _____________________________ <span style="color: white;">{{signature:1:y}}</span>  
+Signature: <span style="color: white;">{{signature:1:y}}</span>  
 Name: {{publisher_manager_name}}  
 Title: {{publisher_manager_title}}  
-Date: _______________ <span style="color: white;">{{date:1:y}}</span>
+Date: <span style="color: white;">{{date:1:y}}</span>
 
 **Writer:**  
 
-Signature: _____________________________ <span style="color: white;">{{signature:2:y}}</span>  
+Signature: <span style="color: white;">{{signature:2:y}}</span>  
 Name: {{writer_full_name}}  
-Date: _______________ <span style="color: white;">{{date:2:y}}</span>
+Date: <span style="color: white;">{{date:2:y}}</span>
 
 **Exhibit A: Compositions**
 
